78a026c31fe54ce1c3f97504a3323d60c119512d
Anderson VanLandingham <vanlandinghamanderson@gmail.com>
2025-06-08 20:26:08 -0400

    Login and Registration pages implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.debug = True
